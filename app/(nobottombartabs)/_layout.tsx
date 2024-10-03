c61fd83b36edb942c16dd40ee4d8fbf2b6b4a35d
Pratik <pratikyadav3949@gmail.com>
2024-10-03 13:36:17 +0530

    feat: add stack expo routes

---
+++ app/(nobottombartabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function RootLayout() {
+  return (
+    <View style={{ backgroundColor: "black" }}>
+      <Text style={{ color: "white" }}>Go Back</Text>
+    </View>
+  );
+}
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function explore() {
   return (
     <View>
       <Text>explore</Text>
+      <Link href={"/accountinfo"}>Account Info</Link>
     </View>
   );
 }
