c6e65012a2e43b0a50e396673bb5d4afb0614a4e
Pratik <pratikyadav3949@gmail.com>
2024-10-04 10:06:48 +0530

    feat: start desining topBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,22 +14,27 @@ export default function TabLayout() {
         }}
       />
 
-      <Tabs.Screen
-        name="account"
-        options={{
-          title: "Account",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="cog" color={color} />
-          ),
-        }}
-      />
+      {/* "th": Represents a grid of items, which is often associated with image galleries.
+          "clone": Looks like stacked images, which could convey browsing or exploring wallpapers.
+          "eye": Often represents viewing, which could imply exploring or viewing wallpapers.
+          "search-plus": Indicates zooming in on content, which can be associated with detailed exploration. */}
 
       <Tabs.Screen
         name="explore"
         options={{
           title: "Explore",
           tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="cogs" color={color} />
+            <FontAwesome size={28} name="clone" color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
           ),
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
+import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function foryou() {
   return (
     <View>
       <Text>for you</Text>
+      <Link href={"/library"}>
+        <Text>Library</Text>
+      </Link>
+      <Link href={"/liked"}>
+        <Text>liked</Text>
+      </Link>
+      <Link href={"/suggested"}>
+        <Text>Suggested</Text>
+      </Link>
     </View>
   );
 }
