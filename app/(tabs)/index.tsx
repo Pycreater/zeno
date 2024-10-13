b57b9d7605784fa3315ce05604993c889a4dd502
Pratik <pratikyadav3949@gmail.com>
2024-10-13 16:07:03 +0530

    feat: carousel functionality is done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,114 @@
 import DownloadPicture from "@/components/BottomSheet";
 import { ImageCard } from "@/components/ImageCard";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { useCarousel } from "@/hooks/useCarousel";
 import { useWallpapers, Wallpaper } from "@/hooks/useWallpapers";
 import { useState } from "react";
-import { Image, Text, StyleSheet, View } from "react-native";
+import { Image, Text, StyleSheet, View, Dimensions } from "react-native";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Carousel from "react-native-reanimated-carousel";
+import { LinearGradient } from "expo-linear-gradient"; // Import LinearGradient
+
+const TOPBAR_HEIGHT = 250;
+const { width } = Dimensions.get("window"); // Define the width of the screen
 
 export default function Explore() {
   const wallpapers = useWallpapers();
-  const [selectedWallapaper, setSelectedWallpaper] = useState<null | Wallpaper>(
+  const [selectedWallpaper, setSelectedWallpaper] = useState<null | Wallpaper>(
     null
   );
+  const carouselItems = useCarousel();
+  const [yOffset, setScrollY] = useState(0);
+
+  const headerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: interpolate(
+            yOffset,
+            [-TOPBAR_HEIGHT, 0, TOPBAR_HEIGHT],
+            [1.5, 1, 1]
+          ),
+        },
+      ],
+    };
+  });
+
+  const textAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(yOffset, [0, 150], [1, 0]),
+    };
+  });
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ParallaxScrollView
         headerBackgroundColor={{ dark: "black", light: "white" }}
         headerImage={
-          <Image
-            style={{ flex: 1 }}
-            source={{
-              uri: wallpapers[0]?.url ?? "",
-            }}
-          />
+          <Animated.View
+            style={[
+              { height: Math.max(0, TOPBAR_HEIGHT - yOffset) },
+              headerAnimatedStyle,
+            ]}
+          >
+            <Carousel
+              width={width}
+              height={TOPBAR_HEIGHT}
+              data={carouselItems}
+              autoPlay
+              autoPlayInterval={2500} // Set the auto-play interval to 2.5 seconds
+              loop // Ensure the carousel loops after the last item
+              onSnapToItem={(index) => console.log("current index:", index)}
+              renderItem={({ index }) => (
+                <>
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Image
+                      source={{ uri: carouselItems[index].image }}
+                      style={{ height: TOPBAR_HEIGHT, width: "100%" }}
+                      resizeMode="cover" // Ensure the image covers the container without distortion
+                    />
+                  </View>
+
+                  <LinearGradient
+                    colors={["transparent", "black"]}
+                    style={{
+                      flex: 1,
+                      position: "absolute",
+                      zIndex: 10,
+                      height: TOPBAR_HEIGHT / 2,
+                      width: "100%",
+                      bottom: 0,
+                    }}
+                  >
+                    <Animated.View style={textAnimatedStyle}>
+                      <Text
+                        style={{
+                          color: "white",
+                          paddingTop: TOPBAR_HEIGHT / 3,
+                          textAlign: "center",
+                          fontSize: 30,
+                          fontWeight: "600",
+                        }}
+                      >
+                        {carouselItems[index].title}
+                      </Text>
+                    </Animated.View>
+                  </LinearGradient>
+                </>
+              )}
+            />
+          </Animated.View>
         }
+        onScroll={(e) => setScrollY(e.nativeEvent.contentOffset.y)} // Capture scroll event to update yOffset
       >
         <View style={styles.container}>
           {wallpapers.map((w: Wallpaper) => (
@@ -31,7 +116,7 @@ export default function Explore() {
               <ImageCard
                 wallpaper={w}
                 onPress={() => {
-                  console.log("seleted wallpaper - " + w);
+                  console.log("selected wallpaper - " + w);
                   setSelectedWallpaper(w);
                 }}
               />
@@ -39,9 +124,9 @@ export default function Explore() {
           ))}
         </View>
       </ParallaxScrollView>
-      {selectedWallapaper && (
+      {selectedWallpaper && (
         <DownloadPicture
-          wallpaper={selectedWallapaper}
+          wallpaper={selectedWallpaper}
           onClose={() => setSelectedWallpaper(null)}
         />
       )}
---
+++ hooks/useCarousel.ts
@@ -0,0 +1,34 @@
+export function useCarousel(): { title: string; image: string }[] {
+  return [
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/etXCrCysR-GYPzAqWaPqHw",
+      title: "Throne",
+    },
+    {
+      title: "Close to me",
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/b0bHDBd4TQeykb89w-ua5Q",
+    },
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/ljlgWoGxROSQuAZEQqdu0g",
+      title: "In here",
+    },
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/8uPhcYvCS7aXaRsqdVUQNQ",
+      title: "Robo-99",
+    },
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/8uPhcYvCS7aXaRsqdVUQNQ",
+      title: "Demon Queen",
+    },
+    {
+      image:
+        "https://ideogram.ai/assets/progressive-image/balanced/response/8uPhcYvCS7aXaRsqdVUQNQ",
+      title: "In here",
+    },
+  ];
+}
